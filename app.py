351a790d9399f41652de46eeadcaabc65229e420
Ashok  Yadav <ashok.yadav@acc.ltd>
2022-04-11 15:14:07 +0530

    Commit form ashok

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ logger.setLevel(logging.DEBUG)
 @app.route('/api/v1/mediaqc/report',methods=['GET'])
 def getQcReport():
 	print("here in printted")
+	print('Ashok Commited')
 
 	videoJson=videoAnalysis()
 	audioJson=audioAnalysis()
