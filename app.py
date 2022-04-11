6032869e5585de6f51da84130d4f439b9ba13b96
Ashok  Yadav <ashok.yadav@acc.ltd>
2022-04-11 15:59:37 +0530

    Ashok

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ logger.setLevel(logging.DEBUG)
 @app.route('/api/v1/mediaqc/report',methods=['GET'])
 def getQcReport():
 	print("here in printted")
-	print('Ashok Commited')
+	print('Ashok Commited again')
+
 
 	videoJson=videoAnalysis()
 	audioJson=audioAnalysis()
