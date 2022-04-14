ab0aee9e264d4d9c58f465214b77d6e7956e88ac
Elton0997 <elton@acc.ltd>
2022-04-14 21:00:17 +0530

    Audio Bit Depth

--- utility/audioAnalysis.py
+++ utility/audioAnalysis.py
@@ -1,11 +1,14 @@
+
 from utility.checks.audio.langDetect import langDetect
 from utility.checks.audio.audiosilenceDetect import audiosilenceDetect
 from utility.checks.audio.bitdepthDetect import bitdepthDetect
 def audioAnalysis(url):
 	langDetectjson=langDetect(url)
-	audiosilenceDetectjson=audiosilenceDetect(url)
+	#audiosilenceDetectjson=audiosilenceDetect(url)
 	bitdepthDetectjson=bitdepthDetect(url)
 
 	audio_report=[]
-	audio_report.append(langDetectjson,audiosilenceDetectjson,bitdepthDetectjson)
+	audio_report.append(langDetectjson)
+	#audio_report.append(audiosilenceDetectjson)
+	audio_report.append(bitdepthDetectjson)
 	return audio_report
