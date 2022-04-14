81a11adfe062420ce908b88b8ecdcd261c829e7b
124 <a124@124s-MacBook-Air.local>
2022-04-14 19:26:29 +0530

    freeze_frame

--- utility/audioAnalysis.py
+++ utility/audioAnalysis.py
@@ -3,8 +3,9 @@ from utility.checks.audio.langDetect import langDetect
 from utility.checks.audio.audiosilenceDetect import audiosilenceDetect
 def audioAnalysis(url):
 	langDetectjson=langDetect(url)
-	audiosilenceDetectjson=audiosilenceDetect(url)
+	#audiosilenceDetectjson=audiosilenceDetect(url)
 
 	audio_report=[]
-	audio_report.append(langDetectjson,audiosilenceDetect)
+	audio_report.append(langDetectjson)
+	#audio_report.append(audiosilenceDetectjson)
 	return audio_report
--- utility/videoAnalysis.py
+++ utility/videoAnalysis.py
@@ -1,11 +1,14 @@
 
 from utility.checks.video.logoDetect import logoDetect
 from utility.checks.video.lightLevelDetect import lightLevelDetect
+from utility.checks.video.freezeFrame import freezeFrame
 def videoAnalysis(url):
 
 	#logoDetectjson=logoDetect()
-	lightLevelDetectjson=lightLevelDetect(url)
+	#lightLevelDetectjson=lightLevelDetect(url)
+	freezeFramejson=freezeFrame(url)
 
 	video_report=[]
-	video_report.append(lightLevelDetectjson)
+	#video_report.append(lightLevelDetectjson)
+	video_report.append(freezeFramejson)
 	return video_report
