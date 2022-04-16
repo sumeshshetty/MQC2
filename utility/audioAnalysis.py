da0ce6d04b9a747aae0f2fb7b41ae08988fb567b
124 <a124@124s-MacBook-Air.local>
2022-04-16 21:29:10 +0530

    Audiolevel and bug fixes

--- utility/audioAnalysis.py
+++ utility/audioAnalysis.py
@@ -2,11 +2,14 @@
 from utility.checks.audio.langDetect import langDetect
 from utility.checks.audio.audiosilenceDetect import audiosilenceDetect
 from utility.checks.audio.bitdepthDetect import bitdepthDetect
+from utility.checks.audio.audioLevel import audioLevel
 def audioAnalysis(url):
 	langDetectjson=langDetect(url)
 	audiosilenceDetectjson=audiosilenceDetect(url)
 
 	bitdepthDetectjson=bitdepthDetect(url)
+	audioLeveljson=audioLevel(url)
+
 
 
 	audio_report=[]
@@ -14,5 +17,6 @@ def audioAnalysis(url):
 	audio_report.append(audiosilenceDetectjson)
 
 	audio_report.append(bitdepthDetectjson)
+	audio_report.append(audioLeveljson)
 
 	return audio_report
