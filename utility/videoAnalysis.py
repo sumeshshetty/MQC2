33d6f98545b6a28a1e784c3301fffbdc0de6101e
Yahya Sirguroh <yahya.sirguroh@acc.ltd>
2022-04-19 12:15:49 +0530

    Duplicate Frame

--- utility/videoAnalysis.py
+++ utility/videoAnalysis.py
@@ -4,6 +4,7 @@ from utility.checks.video.lightLevelDetect import lightLevelDetect
 from utility.checks.video.freezeFrame import freezeFrame
 from utility.checks.video.blackFrame import blackFrame
 from utility.checks.video.defectivePixel import defectivePixel
+from utility.checks.video.DuplicateFrame import DuplicateFrame
 def videoAnalysis(url):
 
 	#logoDetectjson=logoDetect()
@@ -11,11 +12,13 @@ def videoAnalysis(url):
 	freezeFramejson=freezeFrame(url)
 	blackFramejson=blackFrame(url)
 	defectivePixeljson=defectivePixel(url)
+	DuplicateFramejson=DuplicateFrame(url)
 
 	video_report=[]
 	video_report.append(lightLevelDetectjson)
 	video_report.append(freezeFramejson)
 	video_report.append(blackFramejson)
 	video_report.append(defectivePixeljson)
+	video_report.append(DuplicateFramejson)
 
 	return video_report
